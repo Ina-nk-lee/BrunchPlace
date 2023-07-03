e10a8c8b88c61092114225a4dbb80cf33394c57c
Ina Lee <inalee1208@gmail.com>
2023-07-02 19:40:24 -0700

    Added Item and Entree to the model package

---
+++ src/model/Item.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Item {
+    private String name;
+    private double price;
+
+    public Item(String itemName, double itemPrice) {
+        name = itemName;
+        price = itemPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String itemName) {
+        name = itemName;
+    }
+
+    public void setPrice(double itemPrice) {
+        price = itemPrice;
+    }
+}
