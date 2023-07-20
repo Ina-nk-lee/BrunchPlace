8c85017d49e2e8315ac9d83dfac71f8192dc2f0a
Ina Lee <inalee1208@gmail.com>
2023-07-19 19:03:12 -0700

    Added details for the methods in OrderUtil

--- src/model/util/OrderUtil.java
+++ src/model/util/OrderUtil.java
@@ -1,6 +1,56 @@
 package model.util;
 
+import model.itemGroup.Menu;
+import model.itemGroup.Order;
+import java.util.Scanner;
+
 public class OrderUtil extends Util {
-    public OrderUtil() {
+    Scanner intScan;
+    Scanner StrScan;
+    Menu menu;
+    Order order;
+
+    public OrderUtil(Menu newMenu, Order newOrder) {
+        menu = newMenu;
+        order = newOrder;
+
+        this.showOptions();
+        this.readCommand();
+    }
+
+    public void showOptions() {
+        System.out.println("Select a number to proceed.");
+        System.out.println("1. Add Item");
+        System.out.println("2. Remove Item");
+        System.out.println("3. Order");
+        System.out.println("Press any other button to return to the Main Menu.");
+    }
+
+    public void readCommand() {
+        intScan = new Scanner(System.in);
+        int command = intScan.nextInt();
+
+        switch(command) {
+            case 1:
+                this.addItem();
+            case 2:
+                this.removeItem();
+            case 3:
+                this.makeOrder();
+            default:
+                break;
+        }
+    }
+
+    public void addItem() {
+
+    }
+
+    public void removeItem() {
+
+    }
+
+    public void makeOrder() {
+
     }
 }
