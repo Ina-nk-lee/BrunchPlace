894b89d8c9af651700af518a272c4ae4e79e63bf
Ina Lee <inalee1208@gmail.com>
2023-08-05 19:33:50 -0700

    Added Javadoc descriptions.

--- src/model/group/Group.java
+++ src/model/group/Group.java
@@ -5,30 +5,57 @@ import model.single.Item;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class represents a group of items.
+ */
 public class Group {
     protected List<Item> items;
 
+    /**
+     * Creates an item group.
+     */
     public Group() {
         items = new ArrayList<>();
     }
 
+    /**
+     * Adds an item to this group.
+     * @param item to be added.
+     */
     public void addItem(Item item) {
         items.add(item);
     }
 
+    /**
+     * Removes an item from this group.
+     * @param item to be removed.
+     */
     public void removeItem(Item item) {
         items.remove(item);
     }
 
+    /**
+     * Gets the number of items in this group.
+     * @return the number of items in this group.
+     */
     public int numItem() {
         return items.size();
     }
 
+    /**
+     * Gets the list of items in this group.
+     * @return the list of items in this group.
+     */
     protected List<Item> getItems() {
         return items;
     }
 
-    //  Converts Items to String starting with a headline and by placing each item on a new line.
+    /**
+     * Converts items in this group to a string.
+     * A given headline comes first as a title of this group.
+     * @param headline is a title of the string.
+     * @return a string of items in this group.
+     */
     public String toString(String headline) {
         StringBuilder result = new StringBuilder();
         result.append("<").append(headline).append(">").append("\n");
--- src/model/group/Menu.java
+++ src/model/group/Menu.java
@@ -1,21 +1,40 @@
 package model.group;
 
+/**
+ * This class represents a menu of items.
+ */
 public class Menu extends Group {
     private String name;
 
+    /**
+     * Creates a menu with a given name.
+     * @param menuName is the name of the menu.
+     */
     public Menu(String menuName) {
         super();
         name = menuName;
     }
 
+    /**
+     * A getter for the name of this menu.
+     * @return the name of this menu.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * A setter for the name of this menu.
+     * @param newName is the new name of this menu.
+     */
     public void setName(String newName) {
         name = newName;
     }
 
+    /**
+     * Converts this menu into a string.
+     * @return a string that is converted from this menu.
+     */
     public String toString() {
         return super.toString(name);
     }
