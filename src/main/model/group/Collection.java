c7ecc14d8b025b300edc8dca15231fc75cd9ffdf
Ina Lee <inalee1208@gmail.com>
2023-08-07 13:02:27 -0700

    Added Javadoc descriptions.

--- src/main/model/group/Collection.java
+++ src/main/model/group/Collection.java
@@ -3,29 +3,56 @@ package main.model.group;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class represents a collection of item groups.
+ */
 public class Collection {
     protected List<Group> itemGroups;
 
+    /**
+     * Creates a collection object.
+     */
     public Collection() {
         itemGroups = new ArrayList<>();
     }
 
+    /**
+     * Adds a given group to this collection.
+     * @param group to be added.
+     */
     public void addGroup(Group group) {
         itemGroups.add(group);
     }
 
+    /**
+     * Removes a given group from this collection.
+     * @param group to be removed.
+     */
     public void removeGroup(Group group) {
         itemGroups.remove(group);
     }
 
+    /**
+     * A getter for this collection's group list.
+     * @return a list of groups.
+     */
     public List<Group> getList() {
         return itemGroups;
     }
 
+    /**
+     * Gets the size of this collection.
+     * @return the size of this collection.
+     */
     public int numItems() {
         return itemGroups.size();
     }
 
+    /**
+     * Converts item groups in this collection into a string.
+     * The elements are numbered.
+     * @return a string of the item groups in this collection.
+     */
     public String toString() {
         StringBuilder result = new StringBuilder();
         int i = 1;
