7a326705145c5bb572b1ad92033594e0148ac764
Ina Lee <inalee1208@gmail.com>
2023-08-25 21:11:54 -0700

    Separated ButtonPane.java from OrderPane.java for the enhanced OOP structure.

--- src/main/ui/TakeOrderPane.java
+++ src/main/ui/TakeOrderPane.java
@@ -51,59 +51,6 @@ public class TakeOrderPane extends JSplitPane {
         setRightComponent(rightPanel);
     }
 
-//    /**
-//     * Sets a tab for each menu.
-//     */
-//    private void setTabs() {
-//        tabPanel = new JTabbedPane();
-//        for(Menu menu : menus) {
-//            tabPanel.addTab(menu.getName(), setMenuPane(menu));
-//        }
-//    }
-//
-//    /**
-//     * Sets a menu pane, which goes into a tab panel.
-//     * It shows all the menu items in the menu as buttons.
-//     * An item panel that shows all the item buttons in a grid goes on top of a JScrollPane.
-//     */
-//    private JScrollPane setMenuPane(Menu menu) {
-//        // preferred button height = 40
-//        // preferred button width = 108
-//
-//        JPanel itemPanel = new JPanel();
-//        itemPanel.setLayout(new GridBagLayout());
-//
-//        int x = 0;
-//        int y = 0;
-//
-//        for(Item item : menu) {
-//            GridBagConstraints constraints = new GridBagConstraints();
-//
-//            //  Place three items in a row.
-//            if(x % 3 == 0) {
-//                x = 0;
-//                y++;
-//            }
-//
-//            constraints.gridx = x;
-//            constraints.gridy = y;
-//            constraints.weightx = 1;
-//            constraints.fill = GridBagConstraints.HORIZONTAL;
-//
-//            ItemButton button = new ItemButton(item, buttonHandler);
-//            itemButtons.add(button);
-//            itemPanel.add(button, constraints);
-//
-//            x++;
-//        }
-//
-//        JScrollPane menuPane = new JScrollPane(itemPanel);
-//        menuPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-//        menuPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-//
-//        return menuPane;
-//    }
-
     /**
      * Sets a list panel, which shows all the items in the cart, and an order button.
      * Cart items in a JScrollPane are on the top, and the order button is at the bottom.
